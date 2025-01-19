3f7124e6d1a3bb10889a4dd4f46e1f663811de5d
jemish kevadiya <jemishkevadiya007@gmail.com>
2025-01-18 21:26:33 -0500

    Final code with filtering

--- filter_countries.js
+++ filter_countries.js
@@ -7,6 +7,10 @@ const usaFile = 'usa.txt';
 
 const filesToDelete = ['canada.txt', 'usa.txt'];
 
+// Create write streams for canada.txt & usa.txt
+const canadaStream = fs.createWriteStream(canadaFile);
+const usaStream = fs.createWriteStream(usaFile);
+
 // delete files if they exist
 filesToDelete.forEach((file) => {
     if (fs.existsSync(file)) {
@@ -16,4 +20,29 @@ filesToDelete.forEach((file) => {
         console.log(`${file} doesn't exist.`);
     }
 });
+
+// write header to the output files
+canadaStream.write('country,year,population\n');
+usaStream.write('country,year,population\n');
+
+// Read file and filter rows
+fs.createReadStream(dataFile)
+    .pipe(csv())
+    .on('data', (row) => {
+        const { country } = row;
+        if (country === 'Canada') {
+            canadaStream.write(`${Object.values(row).join(',')}\n`);
+        } else if (country === 'United States') {
+            usaStream.write(`${Object.values(row).join(',')}\n`);
+        }
+    })
+    .on('end', () => {
+        console.log(`Data processing completed.`);
+        canadaStream.end();
+        usaStream.end();
+    })
+    .on('error', (err) => {
+        console.error('Error reading the CSV file:', err);
+    });
+
 
